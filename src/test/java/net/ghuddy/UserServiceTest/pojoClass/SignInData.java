a64b2473dd0996104ac37e54326d0c53c4bf6919
Rifat Bin Siraj <rifatrabbi024@gmail.com>
2022-06-17 23:22:06 +0600

    fixed

--- src/test/java/net/ghuddy/UserServiceTest/pojoClass/SignInData.java
+++ src/test/java/net/ghuddy/UserServiceTest/pojoClass/SignInData.java
@@ -2,10 +2,10 @@ package net.ghuddy.UserServiceTest.pojoClass;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Data
+
 public class SignInData {
     @JsonProperty("password")
     private String password;
